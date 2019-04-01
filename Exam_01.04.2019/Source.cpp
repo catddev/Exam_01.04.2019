68498e13fc625b87d78d4ba9e7c681692e6708fe
Динара Айкенова <@local.itstep>
2019-04-01 09:15:42 +0600

    Добавьте файлы проекта.

---
+++ Exam_01.04.2019/Source.cpp
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<Windows.h>
+#include<ctime>
+#include<cstring>
+#include<fstream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+	while (true)
+	{
+		cout << "Задача № ";
+		cin >> tn; cin.get();
+
+		switch (tn)
+		{
+		case 1:
+		{
+
+		}
+		break;
+		case 2:
+		{
+
+		}
+		break;
+		case 3:
+		{
+
+		}
+		break;
+		default:
+			cout << "нет такой задачи" << endl << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
