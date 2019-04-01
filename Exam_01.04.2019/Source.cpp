2dbd73a249530db2cf7f8f609350130891f5c72b
айкеновад <@205_11>
2019-04-01 10:23:31 +0600

    -

--- Exam_01.04.2019/Source.cpp
+++ Exam_01.04.2019/Source.cpp
@@ -6,11 +6,100 @@
 #include<ctime>
 #include<cstring>
 #include<fstream>
-
 using namespace std;
 
+#pragma region
+//array
+void createArray(int *&a, int n) {
+	a = new int[n];
+}
+void fillArray(int *a, int n) {
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % 5-1;
+}
+void fillArray2(int *a, int n) {
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % 100;
+}
+void printArray(const int *a, int n) {
+	for (int i = 0; i < n; i++)
+		cout << setw(3) << a[i] << " ";
+	cout << endl;
+}
+void deleteArray(int *&a) {
+	delete[] a;
+}
+//matrix
+void fill_matrix(int **pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			pp[i][j] = rand() % 21;
+}
+void print_matrix(int**pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << setw(3) << left << pp[i][j] << " ";
+		}
+	cout << endl;
+	}
+	cout << endl;
+}
+void delete_matrix(int**pp, int n, int m) {
+	for (int i = 0; i < n; i++)
+		delete[] pp[i];
+	delete[] pp;
+}
+#pragma endregion
+//2.	Написать функцию, которая принимает указатель на массив и количество элементов.
+// Функция сжимает массив, удаляя из него элементы равные 0.
+// Возвращает указатель на новый массив, не содержащий 0.
+// case 1
+void erase_zeros(int *&a, int &n) {
+	int *tmp;
+	int k = 0;
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] != 0)
+			k++;
+	}
+	tmp = new int[k];
 
+	for (int i = 0; i < n; i++)
+		if (a[i] != 0)
+			tmp[j++] = a[i];
 
+	delete[] a;
+	a = tmp;
+	n = k;
+}
+//4.	Описать функцию IsPowerN(K, N) логического типа,
+// возвращающую TRUE, если целый параметр K(> 0) является степенью числа N(> 1),
+// FALSE в противном случае.
+// Дано число N(> 1) и набор из 10 целых положительных чисел.
+// С помощью функции IsPowerN найти количество степеней числа N в данном наборе.
+// case 2
+//bool IsPowerN(int k, int n) {
+//	if (k > 0 && n > 1)
+//	{
+//		for (int i = 1; i <= k; i*=n)
+//		{
+//			if (i == k) return true;
+//			if (i > k) return false;
+//		}
+//	}
+//	else
+//		return false;
+//}
+// 5.	Дана матрица размера M × N и целое число K(1 ≤ K ≤ N).
+// Вывести элементы K - го столбца данной матрицы.
+// case 3
+void printColumn(int **pp, int n, int k) {
+	for(int i=0; i<n; i++)
+			cout << pp[i][k-1] << endl;
+}
 
 int main()
 {
@@ -28,18 +117,57 @@ int main()
 		switch (tn)
 		{
 		case 1:
+			
 		{
-
+			int *p;
+			int n;
+			cout << "Enter a number of elements" << endl;
+			cin >> n;
+			createArray(p, n);
+			fillArray(p, n);
+			printArray(p, n);
+			cout << endl << endl;
+			erase_zeros(p, n);
+			printArray(p, n);
+			deleteArray(p);
 		}
 		break;
 		case 2:
 		{
-
+	/*		int n;
+			int *p;
+			cout << "Enter N>1:" << endl;
+			cin >> n;
+			createArray(p, 10);
+			fillArray2(p, 10);
+			printArray(p, 10);
+			int j = 0;
+			for (int i = 0; i < 10; i++)
+			{
+				if (IsPowerN(p[i], n)==true)
+				{
+					cout << p[i] << " ";
+					j++;
+				}
+			}
+				cout << endl << j << endl;*/
 		}
 		break;
 		case 3:
 		{
-
+			int n, m;
+			cout << "Enter number of rows and columns:" << endl;
+			cin >> n >> m;
+			int **pp = new int*[n];
+			for (int i = 0; i < n; i++)
+				pp[i] = new int[m];
+			fill_matrix(pp, n, m);
+			print_matrix(pp, n, m);
+			int k;
+			cout << "Enter number of the column to print K(1 <= K <= N)" << endl;
+			cin >> k;
+			cout << endl;
+			printColumn(pp, n, k);
 		}
 		break;
 		default:
